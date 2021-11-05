d8d5b25dc1d0eba3f16317668d49937571b970b1
Pratham Sharma <prrrathm@gmail.com>
2021-11-05 23:34:58 +0530

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React and TypeScript Learn.</h1>
+      <Button variant='contained' onClick={()=>setCount(count+1)}>{count}</Button>
+
     </div>
   );
 }
