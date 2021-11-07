d19854218ea78762c24ed64428d79ea53686bcc2
Pratham Sharma <prrrathm@gmail.com>
2021-11-07 18:05:19 +0530

    please save my streak

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, Typography, Rating, Box } from '@mui/material';
 import { useState } from 'react';
-
+import '@fontsource/roboto/300.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [value, setValue] = useState<number | null>(2)
   return (
-    <div className="App">
-      <h1>React and TypeScript Learn.</h1>
+    <Box>
+      <Typography variant='h1' fontWeight='600'>React x TypeScript</Typography>
       <Button variant='contained' onClick={()=>setCount(count+1)}>{count}</Button>
+      <br />
 
-    </div>
+      <Typography variant='h2' fontWeight='500'>Star Rating</Typography>
+      <Typography variant='h3'>Star Rating</Typography>
+      <Rating name='star'></Rating>
+      <Typography variant='h3'>Taking value out</Typography>
+      <Rating name='output' value={value} onChange={(event, newValue) => {setValue(newValue);}}></Rating>
+      <Typography variant='staroutput'></Typography>
+    </Box>
   );
 }
 
