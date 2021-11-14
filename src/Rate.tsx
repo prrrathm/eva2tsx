77543a1e98b2b44f0d0ca85c894a4c17acfbca76
Pratham Sharma <prrrathm@gmail.com>
2021-11-15 03:21:29 +0530

    api fetch

--- src/Rate.tsx
+++ src/Rate.tsx
@@ -1,18 +1,19 @@
-import { Button, Typography, Rating, Box } from '@mui/material';
-import { useState } from 'react';
+import { Button, Typography, Rating, Box } from '@mui/material'
+import { useState } from 'react'
 
 function Rate(){
+    const [value, setValue] = useState<number | null>(2)
     return (
-        const [value, setValue] = useState<number | null>(2)
-        <Box>
-            <Typography variant='h2' fontWeight='500'>Star Rating</Typography>
-            <Typography variant='h3'>Star Rating</Typography>
+        <Box textAlign='left' sx={{p:1, border:'2px dashed black'}}>
+            <Typography variant='h3' sx={{m:2,p:2,display:'inline-block'}}>
+                Star Rating
+            </Typography>
             <Rating name='star'></Rating>
-            <Typography variant='h3'>Taking value out</Typography>
-            <Rating name='output' value={value} onChange={(event, newValue) => {setValue(newValue);}}></Rating>
-            <Typography variant='h3'>Output</Typography>
+            <br />
+            <Rating name='output' value={value} sx={{m:2,p:1,}} onChange={(event, newValue) => {setValue(newValue);}}></Rating>
+            <Typography variant='h3' sx={{m:2,p:1}} >Output : {value}</Typography>
         </Box>        
     )
 }
 
-export default Rate
+export default Rate;
