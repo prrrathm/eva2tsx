8be3a568ef6192701a45ecb3e65737d23adf9a6e
Pratham Sharma <prrrathm@gmail.com>
2021-11-17 00:51:11 -0500

    aster

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import './App.css';
-import { Button, Typography, Rating, Box } from '@mui/material';
-import { useState } from 'react';
-// import '@fontsource/roboto/300.css';
+import { Button, Typography, Box } from '@mui/material';
+import { useState, useEffect } from 'react';
 import "@fontsource/abeezee"
-import Rate from './Rate'
+// import '@fontsource/roboto/300.css';
+// import Rate from './Rate'
+
 function App() {
+
+  const [name, setName] = useState({title : '', first : '', last : ''})
+  const fetchData = async () =>{
+    const randData = await fetch('https://randomuser.me/api/')
+    const json = await randData.json()
+    setName( json.results[0].name)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // const rand = useFetch("https://randomuser.me/api/")
   const [count, setCount] = useState(0)
-  const [value, setValue] = useState<number | null>(2)
   return (
     <Box sx={{fontFamily:'ABeeZee',textAlign:'center',}}>
-      <Typography variant='h1' fontWeight='600'>React x TypeScript</Typography>
-      <Button variant='contained' onClick={()=>setCount(count+1)}>{count}</Button>
-      <br />
-      <Typography variant='h2' fontWeight='500'>Star Rating</Typography>
-      <Typography variant='h3'>Star Rating</Typography>
-      <Rating name='star'></Rating>
-      <Typography variant='h3'>Taking value out</Typography>
-      <Rating name='output' value={value} onChange={(event, newValue) => {setValue(newValue);}}></Rating>
-      <Typography variant='h3'>Output</Typography>
+      {/* <Typography variant='h1' fontWeight='600'>React x TypeScript</Typography>
+      <br /> */}
+      <Typography variant='h2' sx={{color:'grey'}}>
+        Name : { name.title + ' ' + name.first + ' ' + name.last}
+      </Typography>
     </Box>
   );
 }
